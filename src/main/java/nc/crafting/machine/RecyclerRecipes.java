ddf94550594078204546c8cc914212298ee66544
DrParadox7 <47131096+DrParadox7@users.noreply.github.com>
2021-08-14 13:25:19 +0100

    Declutter NEI
    Remove obsolete machine recipes that have been integrated to the modpack via other methods

--- src/main/java/nc/crafting/machine/RecyclerRecipes.java
+++ src/main/java/nc/crafting/machine/RecyclerRecipes.java
@@ -14,49 +14,49 @@ public class RecyclerRecipes extends NCRecipeHelper {
 	}
 
 	public void addRecipes() {
-		recipe("dLEU235Cell", "tinyU238Base", 24, "tinyNp236Base", 4, "tinyNp237Base", 4, "tinyPu239Base", 8);
-		recipe("dHEU235Cell", "tinyU238Base", 16, "tinyNp237Base", 8, "tinyPu239Base", 8, "tinyPu242Base", 8);
-		recipe("dLEP239Cell", "tinyPu242Base", 24, "tinyPu241Base", 4, "tinyAm242Base", 4, "tinyAm243Base", 8);
-		recipe("dHEP239Cell", "tinyPu242Base", 16, "tinyPu241Base", 8, "tinyAm242Base", 8, "tinyAm243Base", 8);
-		recipe("dMOX239Cell", "tinyU238Oxide", 24, "tinyPu239Oxide", 4, "tinyPu241Oxide", 4, "tinyPu242Oxide", 8);
-		recipe("dTBUCell", "tinyU233Base", 24, "tinyU235Base", 4, "tinyNp236Base", 4, "tinyNp237Base", 8);
-		recipe("dLEU233Cell", "tinyU238Base", 24, "tinyU235Base", 4, "tinyNp236Base", 4, "tinyNp237Base", 8);
-		recipe("dHEU233Cell", "tinyU238Base", 16, "tinyNp236Base", 8, "tinyNp237Base", 8, "tinyPu239Base", 8);
-		recipe("dLEP241Cell", "tinyPu242Base", 24, "tinyAm241Base", 4, "tinyAm242Base", 4, "tinyCm243Base", 8);
-		recipe("dHEP241Cell", "tinyPu242Base", 16, "tinyAm243Base", 8, "tinyCm243Base", 8, "tinyCm246Base", 8);
-		recipe("dMOX241Cell", "tinyU238Oxide", 24, "tinyPu239Oxide", 4, "tinyPu241Oxide", 4, "tinyAm243Oxide", 8);
+		recipe("fueldLEU235Cell", "fueltinyU238Base", 24, "fueltinyNp236Base", 4, "fueltinyNp237Base", 4, "fueltinyPu239Base", 8);
+		recipe("fueldHEU235Cell", "fueltinyU238Base", 16, "fueltinyNp237Base", 8, "fueltinyPu239Base", 8, "fueltinyPu242Base", 8);
+		recipe("fueldLEP239Cell", "fueltinyPu242Base", 24, "fueltinyPu241Base", 4, "fueltinyAm242Base", 4, "fueltinyAm243Base", 8);
+		recipe("fueldHEP239Cell", "fueltinyPu242Base", 16, "fueltinyPu241Base", 8, "fueltinyAm242Base", 8, "fueltinyAm243Base", 8);
+		recipe("fueldMOX239Cell", "fueltinyU238Oxide", 24, "fueltinyPu239Oxide", 4, "fueltinyPu241Oxide", 4, "fueltinyPu242Oxide", 8);
+		recipe("fueldTBUCell", "fueltinyU233Base", 24, "fueltinyU235Base", 4, "fueltinyNp236Base", 4, "fueltinyNp237Base", 8);
+		recipe("fueldLEU233Cell", "fueltinyU238Base", 24, "fueltinyU235Base", 4, "fueltinyNp236Base", 4, "fueltinyNp237Base", 8);
+		recipe("fueldHEU233Cell", "fueltinyU238Base", 16, "fueltinyNp236Base", 8, "fueltinyNp237Base", 8, "fueltinyPu239Base", 8);
+		recipe("fueldLEP241Cell", "fueltinyPu242Base", 24, "fueltinyAm241Base", 4, "fueltinyAm242Base", 4, "fueltinyCm243Base", 8);
+		recipe("fueldHEP241Cell", "fueltinyPu242Base", 16, "fueltinyAm243Base", 8, "fueltinyCm243Base", 8, "fueltinyCm246Base", 8);
+		recipe("fueldMOX241Cell", "fueltinyU238Oxide", 24, "fueltinyPu239Oxide", 4, "fueltinyPu241Oxide", 4, "fueltinyAm243Oxide", 8);
 		
-		recipe("dLEU235CellOxide", "tinyU238Oxide", 24, "tinyNp236Oxide", 4, "tinyNp237Oxide", 4, "tinyPu239Oxide", 8);
-		recipe("dHEU235CellOxide", "tinyU238Oxide", 16, "tinyNp237Oxide", 8, "tinyPu239Oxide", 8, "tinyPu242Oxide", 8);
-		recipe("dLEP239CellOxide", "tinyPu242Oxide", 24, "tinyPu241Oxide", 4, "tinyAm242Oxide", 4, "tinyAm243Oxide", 8);
-		recipe("dHEP239CellOxide", "tinyPu242Oxide", 16, "tinyPu241Oxide", 8, "tinyAm242Oxide", 8, "tinyAm243Oxide", 8);
-		recipe("dTBUCellOxide", "tinyU233Oxide", 24, "tinyU235Oxide", 4, "tinyNp236Oxide", 4, "tinyNp237Oxide", 8);
-		recipe("dLEU233CellOxide", "tinyU238Oxide", 24, "tinyU235Oxide", 4, "tinyNp236Oxide", 4, "tinyNp237Oxide", 8);
-		recipe("dHEU233CellOxide", "tinyU238Oxide", 16, "tinyNp236Oxide", 8, "tinyNp237Oxide", 8, "tinyPu239Oxide", 8);
-		recipe("dLEP241CellOxide", "tinyPu242Oxide", 24, "tinyAm241Oxide", 4, "tinyAm242Oxide", 4, "tinyCm243Oxide", 8);
-		recipe("dHEP241CellOxide", "tinyPu242Oxide", 16, "tinyAm243Oxide", 8, "tinyCm243Oxide", 8, "tinyCm246Oxide", 8);
+		recipe("fueldLEU235CellOxide", "fueltinyU238Oxide", 24, "fueltinyNp236Oxide", 4, "fueltinyNp237Oxide", 4, "fueltinyPu239Oxide", 8);
+		recipe("fueldHEU235CellOxide", "fueltinyU238Oxide", 16, "fueltinyNp237Oxide", 8, "fueltinyPu239Oxide", 8, "fueltinyPu242Oxide", 8);
+		recipe("fueldLEP239CellOxide", "fueltinyPu242Oxide", 24, "fueltinyPu241Oxide", 4, "fueltinyAm242Oxide", 4, "fueltinyAm243Oxide", 8);
+		recipe("fueldHEP239CellOxide", "fueltinyPu242Oxide", 16, "fueltinyPu241Oxide", 8, "fueltinyAm242Oxide", 8, "fueltinyAm243Oxide", 8);
+		recipe("fueldTBUCellOxide", "fueltinyU233Oxide", 24, "fueltinyU235Oxide", 4, "fueltinyNp236Oxide", 4, "fueltinyNp237Oxide", 8);
+		recipe("fueldLEU233CellOxide", "fueltinyU238Oxide", 24, "fueltinyU235Oxide", 4, "fueltinyNp236Oxide", 4, "fueltinyNp237Oxide", 8);
+		recipe("fueldHEU233CellOxide", "fueltinyU238Oxide", 16, "fueltinyNp236Oxide", 8, "fueltinyNp237Oxide", 8, "fueltinyPu239Oxide", 8);
+		recipe("fueldLEP241CellOxide", "fueltinyPu242Oxide", 24, "fueltinyAm241Oxide", 4, "fueltinyAm242Oxide", 4, "fueltinyCm243Oxide", 8);
+		recipe("fueldHEP241CellOxide", "fueltinyPu242Oxide", 16, "fueltinyAm243Oxide", 8, "fueltinyCm243Oxide", 8, "fueltinyCm246Oxide", 8);
 
-		recipe("dLEN236Cell", "tinyNp237Base", 24, "tinyU238Base", 4, "tinyPu239Base", 4, "tinyPu241Base", 8);
-		recipe("dHEN236Cell", "tinyNp237Base", 16, "tinyPu238Base", 8, "tinyPu239Base", 8, "tinyPu241Base", 8);
-		recipe("dLEA242Cell", "tinyAm243Base", 24, "tinyCm243Base", 4, "tinyCm246Base", 4, "tinyCm247Base", 8);
-		recipe("dHEA242Cell", "tinyAm243Base", 16, "tinyCm243Base", 8, "tinyCm245Base", 8, "tinyCm247Base", 8);
-		recipe("dLEC243Cell", "tinyCm246Base", 24, "tinyAm243Base", 4, "tinyCm243Base", 4, "tinyCm245Base", 8);
-		recipe("dHEC243Cell", "tinyCm246Base", 16, "tinyAm243Base", 8, "tinyCm243Base", 8, "tinyCm245Base", 8);
-		recipe("dLEC245Cell", "tinyCm246Base", 24, "tinyCm245Base", 4, "tinyCm247Base", 4, "tinyCf250Base", 8);
-		recipe("dHEC245Cell", "tinyCm246Base", 16, "tinyCm245Base", 8, "tinyCm247Base", 8, "tinyCf250Base", 8);
-		recipe("dLEC247Cell", "tinyCm246Base", 24, "tinyCm247Base", 4, "tinyCm247Base", 4, "tinyCf250Base", 8);
-		recipe("dHEC247Cell", "tinyCm246Base", 16, "tinyCm247Base", 8, "tinyCm247Base", 8, "tinyCf250Base", 8);
+		recipe("fueldLEN236Cell", "fueltinyNp237Base", 24, "fueltinyU238Base", 4, "fueltinyPu239Base", 4, "fueltinyPu241Base", 8);
+		recipe("fueldHEN236Cell", "fueltinyNp237Base", 16, "fueltinyPu238Base", 8, "fueltinyPu239Base", 8, "fueltinyPu241Base", 8);
+		recipe("fueldLEA242Cell", "fueltinyAm243Base", 24, "fueltinyCm243Base", 4, "fueltinyCm246Base", 4, "fueltinyCm247Base", 8);
+		recipe("fueldHEA242Cell", "fueltinyAm243Base", 16, "fueltinyCm243Base", 8, "fueltinyCm245Base", 8, "fueltinyCm247Base", 8);
+		recipe("fueldLEC243Cell", "fueltinyCm246Base", 24, "fueltinyAm243Base", 4, "fueltinyCm243Base", 4, "fueltinyCm245Base", 8);
+		recipe("fueldHEC243Cell", "fueltinyCm246Base", 16, "fueltinyAm243Base", 8, "fueltinyCm243Base", 8, "fueltinyCm245Base", 8);
+		recipe("fueldLEC245Cell", "fueltinyCm246Base", 24, "fueltinyCm245Base", 4, "fueltinyCm247Base", 4, "fueltinyCf250Base", 8);
+		recipe("fueldHEC245Cell", "fueltinyCm246Base", 16, "fueltinyCm245Base", 8, "fueltinyCm247Base", 8, "fueltinyCf250Base", 8);
+		recipe("fueldLEC247Cell", "fueltinyCm246Base", 24, "fueltinyCm247Base", 4, "fueltinyCm247Base", 4, "fueltinyCf250Base", 8);
+		recipe("fueldHEC247Cell", "fueltinyCm246Base", 16, "fueltinyCm247Base", 8, "fueltinyCm247Base", 8, "fueltinyCf250Base", 8);
 		
-		recipe("dLEN236CellOxide", "tinyNp237Oxide", 24, "tinyU238Oxide", 4, "tinyPu239Oxide", 4, "tinyPu241Oxide", 8);
-		recipe("dHEN236CellOxide", "tinyNp237Oxide", 16, "tinyPu238Oxide", 8, "tinyPu239Oxide", 8, "tinyPu241Oxide", 8);
-		recipe("dLEA242CellOxide", "tinyAm243Oxide", 24, "tinyCm243Oxide", 4, "tinyCm246Oxide", 4, "tinyCm247Oxide", 8);
-		recipe("dHEA242CellOxide", "tinyAm243Oxide", 16, "tinyCm243Oxide", 8, "tinyCm245Oxide", 8, "tinyCm247Oxide", 8);
-		recipe("dLEC243CellOxide", "tinyCm246Oxide", 24, "tinyAm243Oxide", 4, "tinyCm243Oxide", 4, "tinyCm245Oxide", 8);
-		recipe("dHEC243CellOxide", "tinyCm246Oxide", 16, "tinyAm243Oxide", 8, "tinyCm243Oxide", 8, "tinyCm245Oxide", 8);
-		recipe("dLEC245CellOxide", "tinyCm246Oxide", 24, "tinyCm245Oxide", 4, "tinyCm247Oxide", 4, "tinyCf250Oxide", 8);
-		recipe("dHEC245CellOxide", "tinyCm246Oxide", 16, "tinyCm245Oxide", 8, "tinyCm247Oxide", 8, "tinyCf250Oxide", 8);
-		recipe("dLEC247CellOxide", "tinyCm246Oxide", 24, "tinyCm247Oxide", 4, "tinyCm247Oxide", 4, "tinyCf250Oxide", 8);
-		recipe("dHEC247CellOxide", "tinyCm246Oxide", 16, "tinyCm247Oxide", 8, "tinyCm247Oxide", 8, "tinyCf250Oxide", 8);
+		recipe("fueldLEN236CellOxide", "fueltinyNp237Oxide", 24, "fueltinyU238Oxide", 4, "fueltinyPu239Oxide", 4, "fueltinyPu241Oxide", 8);
+		recipe("fueldHEN236CellOxide", "fueltinyNp237Oxide", 16, "fueltinyPu238Oxide", 8, "fueltinyPu239Oxide", 8, "fueltinyPu241Oxide", 8);
+		recipe("fueldLEA242CellOxide", "fueltinyAm243Oxide", 24, "fueltinyCm243Oxide", 4, "fueltinyCm246Oxide", 4, "fueltinyCm247Oxide", 8);
+		recipe("fueldHEA242CellOxide", "fueltinyAm243Oxide", 16, "fueltinyCm243Oxide", 8, "fueltinyCm245Oxide", 8, "fueltinyCm247Oxide", 8);
+		recipe("fueldLEC243CellOxide", "fueltinyCm246Oxide", 24, "fueltinyAm243Oxide", 4, "fueltinyCm243Oxide", 4, "fueltinyCm245Oxide", 8);
+		recipe("fueldHEC243CellOxide", "fueltinyCm246Oxide", 16, "fueltinyAm243Oxide", 8, "fueltinyCm243Oxide", 8, "fueltinyCm245Oxide", 8);
+		recipe("fueldLEC245CellOxide", "fueltinyCm246Oxide", 24, "fueltinyCm245Oxide", 4, "fueltinyCm247Oxide", 4, "fueltinyCf250Oxide", 8);
+		recipe("fueldHEC245CellOxide", "fueltinyCm246Oxide", 16, "fueltinyCm245Oxide", 8, "fueltinyCm247Oxide", 8, "fueltinyCf250Oxide", 8);
+		recipe("fueldLEC247CellOxide", "fueltinyCm246Oxide", 24, "fueltinyCm247Oxide", 4, "fueltinyCm247Oxide", 4, "fueltinyCf250Oxide", 8);
+		recipe("fueldHEC247CellOxide", "fueltinyCm246Oxide", 16, "fueltinyCm247Oxide", 8, "fueltinyCm247Oxide", 8, "fueltinyCf250Oxide", 8);
 	}
 	
 	public void recipe(String in, String out1, int num1, String out2, int num2, String out3, int num3, String out4, int num4) {
--- src/main/java/nc/crafting/machine/SeparatorRecipes.java
+++ src/main/java/nc/crafting/machine/SeparatorRecipes.java
@@ -14,25 +14,25 @@ public class SeparatorRecipes extends NCRecipeHelper {
 	}
 
 	public void addRecipes() {
-		fuel("U235", "U238", "U235");
-		fuel("P239", "Pu242", "Pu239");
-		fuel("U233", "U238", "U233");
-		fuel("P241", "Pu242", "Pu241");
+		fuel("fuelU235", "fuelU238", "fuelU235");
+		fuel("fuelP239", "fuelPu242", "fuelPu239");
+		fuel("fuelU233", "fuelU238", "fuelU233");
+		fuel("fuelP241", "fuelPu242", "fuelPu241");
 		
-		recipe("MOX239", "U238Oxide", 8, "Pu239Oxide", 1);
-		recipe("MOX239Cell", "U238Oxide", 8, "Pu239Oxide", 1);
-		recipe("MOX241", "U238Oxide", 8, "Pu241Oxide", 1);
-		recipe("MOX241Cell", "U238Oxide", 8, "Pu241Oxide", 1);
-		recipe("TBU", "Th232Base", 5, "Th232Base", 4);
-		recipe("TBUCell", "Th232Base", 5, "Th232Base", 4);
-		recipe("TBUOxide", "Th232Oxide", 5, "Th232Oxide", 4);
-		recipe("TBUCellOxide", "Th232Oxide", 5, "Th232Oxide", 4);
+		recipe("fuelMOX239", "fuelU238Oxide", 8, "fuelPu239Oxide", 1);
+		recipe("fuelMOX239Cell", "fuelU238Oxide", 8, "fuelPu239Oxide", 1);
+		recipe("fuelMOX241", "fuelU238Oxide", 8, "fuelPu241Oxide", 1);
+		recipe("fuelMOX241Cell", "fuelU238Oxide", 8, "fuelPu241Oxide", 1);
+		recipe("fuelTBU", "fuelTh232Base", 5, "fuelTh232Base", 4);
+		recipe("fuelTBUCell", "fuelTh232Base", 5, "fuelTh232Base", 4);
+		recipe("fuelTBUOxide", "fuelTh232Oxide", 5, "fuelTh232Oxide", 4);
+		recipe("fuelTBUCellOxide", "fuelTh232Oxide", 5, "fuelTh232Oxide", 4);
 		
-		fuel("N236", "Np237", "Np236");
-		fuel("A242", "Am243", "Am242");
-		fuel("C243", "Cm246", "Cm243");
-		fuel("C245", "Cm246", "Cm245");
-		fuel("C247", "Cm246", "Cm247");
+		fuel("fuelN236", "fuelNp237", "fuelNp236");
+		fuel("fuelA242", "fuelAm243", "fuelAm242");
+		fuel("fuelC243", "fuelCm246", "fuelCm243");
+		fuel("fuelC245", "fuelCm246", "fuelCm245");
+		fuel("fuelC247", "fuelCm246", "fuelCm247");
 		
 		recipe("ingotUranium", "U238Base", 2, "tinyU235Base", 2);
 		//recipe("dustUranium", "U238Base", 2, "tinyU235Base", 2);
@@ -75,14 +75,14 @@ public class SeparatorRecipes extends NCRecipeHelper {
 	}
 	
 	public void fuel(String fuel, String fertile, String fissile) {
-		recipe("LE" + fuel, fertile + "Base", 8, fissile + "Base", 1);
-		recipe("HE" + fuel, fertile + "Base", 5, fissile + "Base", 4);
-		recipe("LE" + fuel + "Cell", fertile + "Base", 8, fissile + "Base", 1);
-		recipe("HE" + fuel + "Cell", fertile + "Base", 5, fissile + "Base", 4);
+		recipe("fuelLE" + fuel, fertile + "Base", 8, fissile + "Base", 1);
+		recipe("fuelHE" + fuel, fertile + "Base", 5, fissile + "Base", 4);
+		recipe("fuelLE" + fuel + "Cell", fertile + "Base", 8, fissile + "Base", 1);
+		recipe("fuelHE" + fuel + "Cell", fertile + "Base", 5, fissile + "Base", 4);
 		
-		recipe("LE" + fuel + "Oxide", fertile + "Oxide", 8, fissile + "Oxide", 1);
-		recipe("HE" + fuel + "Oxide", fertile + "Oxide", 5, fissile + "Oxide", 4);
-		recipe("LE" + fuel + "CellOxide", fertile + "Oxide", 8, fissile + "Oxide", 1);
-		recipe("HE" + fuel + "CellOxide", fertile + "Oxide", 5, fissile + "Oxide", 4);
+		recipe("fuelLE" + fuel + "Oxide", fertile + "Oxide", 8, fissile + "Oxide", 1);
+		recipe("fuelHE" + fuel + "Oxide", fertile + "Oxide", 5, fissile + "Oxide", 4);
+		recipe("fuelLE" + fuel + "CellOxide", fertile + "Oxide", 8, fissile + "Oxide", 1);
+		recipe("fuelHE" + fuel + "CellOxide", fertile + "Oxide", 5, fissile + "Oxide", 4);
 	}
 }
